648de321d714d3c9cb04deef05cdef0604be6e11
marianita13 <marianitacero.1913@gmail.com>
2023-10-16 00:23:12 -0500

    feat: :zap: Controller

--- APIVET/Controllers/CiudadController.cs
+++ APIVET/Controllers/CiudadController.cs
@@ -1,16 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using Core.entities;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace APIVET.Controllers
 {
 
-    public class CiudadController : Controller
+    public class CiudadController : BaseController
     {
-        
+        private readonly IUnitOfWork unitOfWork;
+        private IUnitOfWork _UnitOfWork;
+        private IMapper _mapper;
+        public CiudadController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _UnitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<IEnumerable<Ciudad>>> Get(){
+            var ciudades = await _UnitOfWork.Ciudades.GetAllAsync();
+            return Ok(ciudades);
+        }
     }
 }
--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -5,8 +5,18 @@ using System.Threading.Tasks;
 
 namespace Core.Interfaces
 {
-    public class IUnitOfWork 
+    public interface IUnitOfWork
     {
-        
+        ICita Citas {get;}
+        ICiudad Ciudades {get;}
+        ICliente Clientes {get;}
+        IClienteDir ClienteDirecciones {get;}
+        IClienteTel ClienteTelefonos {get;}
+        IDepartamento Departamentos {get;}
+        IMascota Mascotas {get;}
+        IPais Paises {get;}
+        IRaza Razas {get;}
+        IServicio Servicios {get;}
+        Task<int> SaveAsync();
     }
 }

f301c8539d2aa7cecd3adea924433acb38a97f25
marianita13 <marianitacero.1913@gmail.com>
2023-10-17 07:20:21 -0500

    feat: :zap: Controller Ciudad & Departamento

--- APIVET/Controllers/CiudadController.cs
+++ APIVET/Controllers/CiudadController.cs
@@ -1,3 +1,4 @@
+using APIVET.Dtos;
 using AutoMapper;
 using Core.entities;
 using Core.Interfaces;
@@ -8,7 +9,6 @@ namespace APIVET.Controllers
 
     public class CiudadController : BaseController
     {
-        private readonly IUnitOfWork unitOfWork;
         private IUnitOfWork _UnitOfWork;
         private IMapper _mapper;
         public CiudadController(IUnitOfWork unitOfWork, IMapper mapper)
@@ -21,9 +21,73 @@ namespace APIVET.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
 
-        public async Task<ActionResult<IEnumerable<Ciudad>>> Get(){
+        public async Task<ActionResult<IEnumerable<CiudadDto>>> Get(){
             var ciudades = await _UnitOfWork.Ciudades.GetAllAsync();
             return Ok(ciudades);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<CiudadDto>> GetId(int id){
+            var ciudades = await _UnitOfWork.Ciudades.GetIdAsync(id);
+            if (ciudades == null){
+                return NotFound();
+            }
+            return Ok(ciudades);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<CiudadDto>> Post(CiudadDto ciudadDto){
+            var ciudades = _mapper.Map<Ciudad>(ciudadDto);
+            _UnitOfWork.Ciudades.Add(ciudades);
+            await _UnitOfWork.SaveAsync();
+            if (ciudades == null){
+                return BadRequest();
+            }
+            ciudadDto.Id = ciudades.Id;
+            return CreatedAtAction(nameof(Post), new {id = ciudadDto.Id}, ciudadDto); 
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<CiudadDto>> Put(CiudadDto ciudadDto, int id){
+            if (ciudadDto == null){
+                return NotFound();
+            }
+            if (ciudadDto.Id == 0){
+                ciudadDto.Id = id;
+            }
+            if (ciudadDto.Id != id){
+                return BadRequest();
+            }
+
+            var ciudades = _mapper.Map<Ciudad>(ciudadDto);
+            _UnitOfWork.Ciudades.Update(ciudades);
+            await _UnitOfWork.SaveAsync();
+            return ciudadDto;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult> Delete(int id){
+            var ciudades = await _UnitOfWork.Ciudades.GetIdAsync(id);
+            if (ciudades == null){
+                return NotFound();
+            }
+            _UnitOfWork.Ciudades.Remove(ciudades);
+            await _UnitOfWork.SaveAsync();
+            return NoContent();
         }
     }
 }
--- APIVET/Controllers/DepartamentoController.cs
+++ APIVET/Controllers/DepartamentoController.cs
@@ -3,13 +3,94 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using APIVET.Dtos;
+using AutoMapper;
+using Core.entities;
+using Core.Interfaces;
+using Infraestructura.UnitOfWork;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace APIVET.Controllers
 {
-    public class DepartamentoController : Controller
+    public class DepartamentoController : BaseController
     {
-        
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public DepartamentoController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<IEnumerable<DepartamentoDto>>> Get(){
+            var departamento = await _unitOfWork.Departamentos.GetAllAsync();
+            return Ok(departamento);   
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<DepartamentoDto>> GetId(int id){
+            var departamento = await _unitOfWork.Departamentos.GetIdAsync(id);
+            if (departamento == null){
+                return NotFound();
+            }
+            return Ok(departamento);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<DepartamentoDto>> Post(DepartamentoDto departamentoDto, int id){
+            var departamento = _mapper.Map<Departamento>(departamentoDto);
+            if (departamento == null){
+                return BadRequest();
+            }
+            _unitOfWork.Departamentos.Add(departamento);
+            await _unitOfWork.SaveAsync();
+            return CreatedAtAction(nameof(Post), new {id = departamentoDto.Id}, departamentoDto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<DepartamentoDto>> Put(int id, DepartamentoDto departamentoDto){
+            if (departamentoDto.Id == 0){
+                departamentoDto.Id = id;
+            }
+            if (departamentoDto.Id != id){
+                return BadRequest();
+            }
+            if (departamentoDto == null){
+                return NotFound();
+            }
+            var departamento = _mapper.Map<Departamento>(departamentoDto);
+            _unitOfWork.Departamentos.Update(departamento);
+            await _unitOfWork.SaveAsync();
+            return departamentoDto;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult> Delete(int id){
+            var departamento = await _unitOfWork.Departamentos.GetIdAsync(id);
+            if (departamento == null){
+                return NotFound();
+            }
+            _unitOfWork.Departamentos.Remove(departamento);
+            await _unitOfWork.SaveAsync();
+            return NoContent();
+        }
     }
 }
